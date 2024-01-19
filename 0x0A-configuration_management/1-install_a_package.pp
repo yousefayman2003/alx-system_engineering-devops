deccfd2a7838702c9bbcda817752818337a623ce
yousefayman <yousefayman20003@gmail.com>
2024-01-19 11:45:15 -0800

    upload tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # a puppet script that install flask from pip3
 
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => '/usr/bin',
+  refreshonly => true,
 }
