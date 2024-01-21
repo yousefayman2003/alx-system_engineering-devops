3da88768653ff1e3e70537dea22523f16ed01463
yousefayman <yousefayman20003@gmail.com>
2024-01-21 08:28:41 -0800

    upload tasks

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# Configure using puppet
+file_line {'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no'
+}
+
+file_line {'Turn on auth with pk':
+	path => '/etc/ssh/ssh_config',
+	line => 'IdentityFile ~/.ssh/school'
+}
