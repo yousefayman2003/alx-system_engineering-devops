511144c428519c866831b8365bb9209c353da372
yousefayman <yousefayman20003@gmail.com>
2024-02-19 08:49:50 -0800

    upload task 3

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,38 +4,38 @@
 Python script that exports data in the JSON format.
 """
 
-import json
 from requests import get
 from sys import argv
+import json
 
 if __name__ == "__main__":
-    todos_response = get('https://jsonplaceholder.typicode.com/todos/')
-    todos_data = todos_response.json()
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
 
-    user_row = []
-    users_response = get('https://jsonplaceholder.typicode.com/users')
-    users_data = users_response.json()
+    row = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
-    for user_data in users_data:
-        if user_data['id'] == int(argv[1]):
-            username = user_data['username']
-            user_id = user_data['id']
+    for i in data2:
+        if i['id'] == int(argv[1]):
+            u_name = i['username']
+            id_no = i['id']
 
-    user_row = []
+    row = []
 
-    for todo_data in todos_data:
+    for i in data:
 
-        todo_dict = {}
+        new_dict = {}
 
-        if todo_data['userId'] == int(argv[1]):
-            todo_dict['username'] = username
-            todo_dict['task'] = todo_data['title']
-            todo_dict['completed'] = todo_data['completed']
-            user_row.append(todo_dict)
+        if i['userId'] == int(argv[1]):
+            new_dict['username'] = u_name
+            new_dict['task'] = i['title']
+            new_dict['completed'] = i['completed']
+            row.append(new_dict)
 
-    final_user_dict = {}
-    final_user_dict[user_id] = user_row
-    json_object = json.dumps(final_user_dict)
+    final_dict = {}
+    final_dict[id_no] = row
+    json_obj = json.dumps(final_dict)
 
-    with open(argv[1] + ".json",  "w") as json_file:
-        json_file.write(json_object)
+    with open(argv[1] + ".json",  "w") as f:
+        f.write(json_obj)
