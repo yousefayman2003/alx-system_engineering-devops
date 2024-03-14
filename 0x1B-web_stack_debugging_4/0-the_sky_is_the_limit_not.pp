f139277592cf8014681063d92732d058aae9e88e
Yousef Ayman <yousefayman20003@gmail.com>
2024-03-14 14:00:46 -0700

    upload tasks

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,11 @@
 # increase the amount of traffic that nginx server can handle
 
-# increase the U LIMIT
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+# increase ulimit
+exec { 'fix-config-nginx':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
 
 # restart nginx
 exec { 'nginx-restart':
